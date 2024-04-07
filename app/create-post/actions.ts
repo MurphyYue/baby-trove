8fc506eb99710547c57080c8f0a94b18844fc701
murphyyue <murphyyue@icloud.com>
2024-04-07 22:31:36 +0800

    add aws s3 storage

---
+++ app/create-post/actions.ts
@@ -0,0 +1,61 @@
+"use server"
+
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import crypto from "crypto";
+
+const allowedFileTypes = ["image/jpeg", "image/png", "video/mp4", "video/quicktime"];
+
+const maxFileSize = 1048576 * 10; // 1 MB
+
+const generateFileName = (bytes = 32) => crypto.randomBytes(bytes).toString("hex");
+
+type SignedURLResponse = Promise<
+  | { failure?: undefined; success: { url: string; id: number } }
+  | { failure: string; success?: undefined }
+>;
+
+type GetSignedURLParams = {
+  fileType: string;
+  fileSize: number;
+  checksum: string;
+};
+console.log(process.env.AWS_ACCESS_KEY!, process.env.AWS_SECRET_ACCESS_KEY!);
+const s3Client = new S3Client({
+  region: process.env.AWS_BUCKET_REGION!,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY!,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+  },
+});
+export const getSignedURL = async ({
+  fileType,
+  fileSize,
+  checksum,
+}: GetSignedURLParams) => {
+  if (!allowedFileTypes.includes(fileType)) {
+    return { failure: "File type not allowed" };
+  }
+
+  if (fileSize > maxFileSize) {
+    return { failure: "File size too large" };
+  }
+  const fileName = generateFileName();
+
+  const putObjectCommand = new PutObjectCommand({
+    Bucket: process.env.AWS_BUCKET_NAME!,
+    Key: fileName,
+    ContentType: fileType,
+    ContentLength: fileSize,
+    ChecksumSHA256: checksum,
+  });
+
+  const url = await getSignedUrl(
+    s3Client,
+    putObjectCommand,
+    { expiresIn: 60 }, // 60 seconds
+  );
+
+  console.log({ success: url });
+  return { success: { url} };
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
-import Image from "next/image";
 import Timeline from "@/components/Timeline";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
     <div className="flex justify-center items-center min-h-screen bg-white">
       <div className="max-w-lg w-full">
+        <Navbar />
         <Timeline />
       </div>
     </div>
