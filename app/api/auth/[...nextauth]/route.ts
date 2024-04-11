3580572f2c7ee1fd8406c508a822df9fa84c3464
murphyyue <murphyyue@icloud.com>
2024-04-11 22:03:43 +0800

    add option type in nextauth route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,16 @@
 import NextAuth from "next-auth";
+import type { NextAuthOptions } from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
 
 export const OPTIONS = {
   providers: [
     GitHubProvider({
-      clientId: process.env.GITHUB_ID || "",
-      clientSecret: process.env.GITHUB_SECRET || "",
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
     }),
   ],
   secret: process.env.SECRET,
-};
+} satisfies NextAuthOptions;
 const authHandler = NextAuth(OPTIONS);
 export { authHandler as GET, authHandler as POST };
 
