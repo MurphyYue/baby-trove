9c315e2937bd63bd0e4c3899bc23308e399f356e
murphyyue <murphyyue@icloud.com>
2024-04-22 16:02:55 +0800

    replace antd with antd-mobile

--- components/ImagePreview.tsx
+++ components/ImagePreview.tsx
@@ -1,22 +1,43 @@
 "use client"
 
-import React from 'react';
-import { Image } from "antd";
+import React, { useState } from 'react';
+import { ImageViewer, Image } from 'antd-mobile';
 
-export default function ImagePreview({images}: {images: string[]}){
- return (
-   <div className="flex flex-wrap">
-     <Image.PreviewGroup
-       preview={{
-         onChange: (current, prev) => console.log(`current index: ${current}, prev index: ${prev}`),
-       }}
-     >
-       {images.map((url) => (
-         <div className="w-24 h-24 overflow-hidden ml-1 mr-2 mt-2 relative" key={url}>
-           <Image src={url} alt="baby image" />
-         </div>
-       ))}
-     </Image.PreviewGroup>
-   </div>
- );
+export default function ImagePreview({ images }: { images: string[] }) {
+  const [visible, setVisible] = useState(false);
+  return (
+    <div className="flex flex-wrap">
+      {images.map((url, index) => (
+        <div
+          className="w-24 h-24 overflow-hidden ml-1 mr-2 mt-2 relative"
+          key={url}
+          onClick={() => {
+            setVisible(true);
+          }}
+        >
+          <Image width={100} height={100} src={url} alt="baby image" fit='cover' />
+        </div>
+      ))}
+      {
+        images.length > 1 ? <ImageViewer.Multi
+          images={images}
+          visible={visible}
+          defaultIndex={0}
+          onClose={() => {
+            setVisible(false);
+          }}
+        /> : <ImageViewer
+          classNames={{
+            mask: 'customize-mask',
+            body: 'customize-body',
+          }}
+          image={images[0]}
+          visible={visible}
+          onClose={() => {
+            setVisible(false)
+          }}
+        />
+      }
+    </div>
+  );
 }
